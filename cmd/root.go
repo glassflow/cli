be225b06e668a8940ebd6d3b272b389524a9bc3b
Kiran <nkirandroid@gmail.com>
2025-10-31 09:25:27 +0100

    feat: add cobra cli scaffolding

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	verbose    bool
+	configPath string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "glassflow",
+	Short: "GlassFlow CLI - Local development environment",
+	Long:  `GlassFlow CLI provides a local development environment with Kind cluster and demo data.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "path to config file (YAML)")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/glassflow/glassflow-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
